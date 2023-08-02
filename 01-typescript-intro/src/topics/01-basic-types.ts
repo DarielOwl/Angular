5f6c48dea6f92087f33a6c07e254ef07e55aa979
DarielOwl <darieldesosa@gmail.com>
2023-08-02 11:49:47 -0300

    12. Tipos basicos y conceptos generales

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,13 @@
+const name: string = 'Darius';
+let hpPoints: number | 'FULL' = 95;
+let attack: number | string = 3000;
+let isAlive: boolean = true
+
+hpPoints = 'FULL';
+
+console.log({
+    name,hpPoints,isAlive,attack
+})
+
+
+export{}
